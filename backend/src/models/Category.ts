2326c82b1a0589db2b8ff92cf632ad16d2dcf38e
Assombradox <maatheuswork@gmail.com>
2026-01-21 04:37:33 -0300

    update admin | edited categorie

--- backend/src/models/Category.ts
+++ backend/src/models/Category.ts
@@ -5,6 +5,9 @@ export interface ICategory extends Document {
     name: string;
     slug: string;
     image: string;
+    title?: string;
+    description?: string;
+    order?: number;
     type: 'category' | 'collection';
 }
 
@@ -13,7 +16,10 @@ const CategorySchema: Schema = new Schema({
     slug: { type: String, required: true, unique: true, index: true },
     name: { type: String, required: true },
     image: { type: String, required: true },
-    type: { type: String, enum: ['category', 'collection'], required: true }
+    title: { type: String },
+    description: { type: String },
+    order: { type: Number },
+    type: { type: String, enum: ['category', 'collection'], required: true, default: 'category' }
 }, {
     timestamps: true
 });
