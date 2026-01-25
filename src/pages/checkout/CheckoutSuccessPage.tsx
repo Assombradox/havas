dfee5b7e140782e53e99156c68d924c2142a3238
Assombradox <maatheuswork@gmail.com>
2026-01-25 07:46:37 -0300

    Update CheckoutSuccessPage.tsx

--- src/pages/checkout/CheckoutSuccessPage.tsx
+++ src/pages/checkout/CheckoutSuccessPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { CheckCircle2, MessageCircle, ShoppingBag, Calendar, MapPin } from 'lucide-react';
 import logo from '../../assets/logo.png';
 
