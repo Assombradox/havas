f4093b07eff244358d94e4145e143b7c59b602ff
Assombradox <maatheuswork@gmail.com>
2026-02-14 07:41:43 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,17 +8,17 @@ const Footer: React.FC = () => {
             <div className="max-w-7xl mx-auto flex flex-col gap-2 text-left">
                 {/* Copyright */}
                 <p className="text-sm font-bold text-gray-900">
-                    © {currentYear} Havaianas Brasil. All Rights Reserved.
+                    © {currentYear} Havainas Brasil. All Rights Reserved.
                 </p>
 
                 {/* Legal Info */}
                 <div className="text-xs text-gray-500 leading-snug">
                     <p className="mb-1">
-                        Razão Social: Alpargatas S.A. • CNPJ: 61.079.117/0001-05 • Endereço: Av. das Nações Unidas, nº 14.261 - Ala A - 10º andar - São Paulo - São Paulo - Brasil - CEP 04794-000
+                        Razão Social: havainas oficial • CNPJ: 81.029.117/0001-09 • Endereço: Av. das Nações Unidas, nº 14.261 - Ala A - 10º andar - Fortaleza - Brasil - CEP 04794-000
                     </p>
 
                     <p className="mb-1">
-                        Fale Conosco: SP (11) 3003-3414 | Outras regiões 0800 7070 566 • Horário de Atendimento: 09h às 18h de segunda a sexta
+                        Fale Conosco: SP (11) 3003-7941 | Outras regiões 0800 3030 566 • Horário de Atendimento: 09h às 18h de segunda a sexta
                     </p>
 
                     <p>
